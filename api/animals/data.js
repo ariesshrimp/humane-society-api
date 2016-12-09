bafffd7216d8220f679967a018424140cf5bf3e8
joefraley <jose.fraley@gmail.com>
2016-12-09 02:31:40 -0800

    DRY up Firebase calls with Ramda.

--- api/animals/data.js
+++ api/animals/data.js
@@ -1,60 +1,60 @@
 'use strict'
+import R from 'ramda'
 import { database } from '../database'
 const data = database.ref(`/animals`)
 
+const unpack = R.compose(
+  R.values,
+  snapshot => snapshot.val()
+)
+
 export const getByID = id => data.child(`${id}`)
   .once(`value`)
-  .then(snapshot => snapshot.val())
+  .then(unpack)
 
 export const getAllSpecies = species => data.orderByChild(`species`)
   .equalTo(species)
   .once(`value`)
-  .then(snapshot => snapshot.val())
-  .then(results => Object.keys(results).map(id => results[id]))
+  .then(unpack)
 
 export const getAllNamed = name => data.orderByChild(`name`)
   .equalTo(name)
   .once(`value`)
-  .then(snapshot => snapshot.val())
-  .then(results => Object.keys(results).map(id => results[id]))
+  .then(unpack)
 
 export const getCheaperThan = maxPrice => data.orderByChild(`adopt_fee`)
   .endAt(maxPrice)
   .once(`value`)
-  .then(snapshot => snapshot.val())
-  .then(results => Object.keys(results).map(id => results[id]))
+  .then(unpack)
 
 export const getAllOfSex = sex => data.orderByChild(`sex`)
   .equalTo(sex)
   .once(`value`)
-  .then(snapshot => snapshot.val())
-  .then(results => Object.keys(results).map(id => results[id]))
+  .then(unpack)
 
 export const getYoungerThan = age => data.orderByChild(`age`)
   .endAt(age)
   .once(`value`)
-  .then(snapshot => snapshot.val())
-  .then(results => Object.keys(results).map(id => results[id]))
+  .then(unpack)
 
 export const getOlderThan = age => data.orderByChild(`age`)
   .startAt(age)
   .once(`value`)
-  .then(snapshot => snapshot.val())
-  .then(results => Object.keys(results).map(id => results[id]))
+  .then(unpack)
 
 export const sortByDateAvailable = () => data.orderByChild(`date_available`)
   .once(`value`)
-  .then(snapshot => snapshot.val())
-  .then(results => Object.keys(results).map(id => results[id]))
+  .then(unpack)
 
 export const getWithImage = () => data.orderByChild(`image_url`)
   .once(`value`)
   .then(snapshot => snapshot.val())
-  .then(results => results.filter(animal => animal.image_url))
-  .then(results => Object.keys(results).map(id => results[id]))
+  .then(R.compose(
+    R.filter(animal => animal.image_url),
+    R.values
+  ))
 
 export const getByBreed = breed => data.orderByChild(`breed`)
   .equalTo(breed)
   .once(`value`)
-  .then(snapshot => snapshot.val())
-  .then(results => Object.keys(results).map(id => results[id]))
+  .then(unpack)
