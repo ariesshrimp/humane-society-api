3d35c8526d64eb55d8b248d12abaf2b486b5fde4
joefraley <jose.fraley@gmail.com>
2016-12-19 23:29:05 -0800

    Include animal query test.

--- api/__tests__/Schema.test.js
+++ api/__tests__/Schema.test.js
@@ -6,122 +6,168 @@ import Schema from '../Schema'
 import uuid from 'node-uuid'
 import { expect } from 'chai'
 import { graphql } from 'graphql'
-import * as Animals from '../animals/data'
-import * as Database from '../users/data'
 import * as TestObjects from './testObjects.js'
 
-describe(`User operations`, () => {
+describe(`Schema`, () => {
+  // anima is used in Animal Operations tests and User Operations tests
   let animalID = `TEST_${uuid.v4()}`
   let testAnimal = R.merge(TestObjects.animal, { id: animalID })
-  const animalRef = app.database().ref(`/animals/${animalID}`)
+  let animalRef = app.database().ref(`/animals/${animalID}`)
 
-  let userID // this is set by Firebase during the "Create a user" test
-
-  beforeEach(function() {
+  before(async function() {
     this.timeout(5000)
-    return animalRef.set(testAnimal)
+    await animalRef.set(testAnimal)
   })
 
-  it(`Create a user`, async () => {
-    const mutation = `
-      mutation {
-        createUser(
-          email:"${uuid.v4()}@test.com", 
-          name:"TestGuy", 
-          password:"12345678lowerUPPER&%*"
-        ) {
-          id
-          name
-          favorites {
+  describe(`Animal operation`, () => { 
+    it(`Find an animal by ID`, async () => {
+      const query = `
+        query {
+          getAnimal(
+            id:"${animalID}"
+          ) {
+            id
+            name
+          }
+        }
+      `
+
+      const { data: { getAnimal: actual }} = await graphql(Schema, query)
+      const expected = {
+        id: animalID,
+        name: testAnimal.name
+      }
+
+      expect(actual).to.deep.equal(expected)
+    })
+
+    it(`Allows queries by species`, async () => {
+      const query = `
+        query {
+          getAllSpecies(
+            species:"${testAnimal.species}"
+          ) {
+            id
+            name
+            species
+          }
+        }
+      `
+
+      const { data: { getAllSpecies: actual }} = await graphql(Schema, query)
+      const expected = [{
+        id: animalID,
+        name: testAnimal.name,
+        species: testAnimal.species
+      }]
+
+      expect(actual).to.deep.equal(expected)
+    })
+  })
+
+  describe(`User operations`, () => {
+    let userID
+
+    it(`Create a user`, async () => {
+      const mutation = `
+        mutation {
+          createUser(
+            email:"${uuid.v4()}@test.com", 
+            name:"TestGuy", 
+            password:"12345678lowerUPPER&%*"
+          ) {
+            id
+            name
+            favorites {
+              id
+            }
+          }
+        }
+      `
+      const { data: { createUser: actual }} = await graphql(Schema, mutation)
+      userID = actual.id // save this id for us in the rest of the user test suite
+      
+      const expected = { 
+        id: userID,
+        favorites: [],
+        name: `TestGuy`
+      }
+
+      expect(actual).to.deep.equal(expected)
+    })
+
+    it(`Lets a user mark some animals as favorites`, async () => {
+      const mutation = `
+        mutation {
+          addFavorite(
+            animalId:"${animalID}", 
+            userId:"${userID}"
+          ) {
+            id
+            favorites {
+              id
+              followers {
+                id
+              }
+            }
+          }
+        }
+      `
+      const { data: { addFavorite: actual }} = await graphql(Schema, mutation)
+      const expected = {
+        id: userID, // return the user
+        favorites: [{ // with a list of faves
+          id: animalID, // containing animal
+          followers: [{ // who's followers include
+            id: userID // just this user
+          }]
+        }],
+      }
+
+      expect(actual).to.deep.equal(expected)
+    })
+
+
+    it(`Lets you destroy a user by ID`, async () => {
+      const mutation = `
+        mutation {
+          removeUser(
+            userId:"${userID}"
+          ) {
             id
           }
         }
-      }
-    `
-    const { data: { createUser: actual }} = await graphql(Schema, mutation)
-    userID = actual.id // save this id for us in the rest of the user test suite
-    
-    const expected = { 
-      id: userID,
-      favorites: [],
-      name: `TestGuy`
-    }
+      `
+      const { data: { removeUser: destroyed }} = await graphql(Schema, mutation) // kill the user
+      expect(destroyed.id).to.equal(userID) // should return the deleted user's info
 
-    expect(actual).to.deep.equal(expected)
-  })
-
-  it(`Lets a user mark some animals as favorites`, async () => {
-    const mutation = `
-      mutation {
-        addFavorite(
-          animalId:"${animalID}", 
-          userId:"${userID}"
-        ) {
-          id
-          favorites {
+      const query = `
+        query {
+          getUser(
+            id:"${userID}"
+          ) {
             id
+          }
+        }
+      `
+      const { data: { getUser: actual }} = await graphql(Schema, query) // try and find them afterwards
+      expect(actual).to.be.null // user should no longer be in the database
+
+      const { data: { getAnimal: animal }} = await graphql(Schema, `
+        query {
+          getAnimal(
+            id:"${animalID}"
+          ) {
             followers {
               id
             }
           }
         }
-      }
-    `
-    const { data: { addFavorite: actual }} = await graphql(Schema, mutation)
-    const expected = {
-      id: userID, // return the user
-      favorites: [{ // with a list of faves
-        id: animalID, // containing animal
-        followers: [{ // who's followers include
-          id: userID // just this user
-        }]
-      }],
-    }
+      `)
+      expect(animal.followers).to.be.empty // user removed from all animals they were following
+    })
 
-    expect(actual).to.deep.equal(expected)
+    // Clean-up those test entities we started with
+    after(() => animalRef.remove())
   })
-
-
-  it(`Lets you destroy a user by ID`, async () => {
-    const mutation = `
-      mutation {
-        removeUser(
-          userId:"${userID}"
-        ) {
-          id
-        }
-      }
-    `
-    const { data: { removeUser: destroyed }} = await graphql(Schema, mutation) // kill the user
-    expect(destroyed.id).to.equal(userID) // should return the deleted user's info
-
-    const query = `
-      query {
-        getUser(
-          id:"${userID}"
-        ) {
-          id
-        }
-      }
-    `
-    const { data: { getUser: actual }} = await graphql(Schema, query) // try and find them afterwards
-    expect(actual).to.be.null // user should no longer be in the database
-
-    const { data: { getAnimal: animal }} = await graphql(Schema, `
-      query {
-        getAnimal(
-          id:"${animalID}"
-        ) {
-          followers {
-            id
-          }
-        }
-      }
-    `)
-    expect(animal.followers).to.be.empty // user removed from all animals they were following
-  })
-
-  // Clean-up those test entities we started with
-  after(() => animalRef.remove())
 })
-
